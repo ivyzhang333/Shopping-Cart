03d7cbc074ad3b57c4f2157b513a086278d47a2e
ivyzhang333 <ivyzhang333@gmail.com>
2022-07-13 13:53:18 -0700

    uploading shopping cart project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import Header from './component/Header';
+import Main from './component/Main';
+import Basket from './component/Basket';
+import data from './data';
+import { useState } from 'react';
+import Product from './Product';
 
 function App() {
+  const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
+  const onAdd = (Product) => {
+    const exist = cartItems.find(x => x.id === products.id);
+    if (exist) {
+      setCartItems(cartItems.map(x => x.id === products.id ? { ...exist, qty: exist.qty + 1 } : x));
+    } else {
+      setCartItems([...cartItems, { ...products, qt: 1 }]);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <div className="row">
+        <Main onAdd={onAdd} products={products}></Main>
+        <Basket onAdd={onAdd} cartItems={cartItems}></Basket>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name: 'Macbook',
+            price: 1400,
+            image: 'https://www.cnet.com/a/img/resize/8ac23b08c4b7cb7645aec9fdcf026af94e6b7159/hub/2015/04/07/b35f1174-696c-4d25-8a4f-fa16a2842338/macbook-air-gold-2015-16.jpg?auto=webp&width=768',
+        },
+        {
+            id: '2',
+            name: 'Old Car',
+            price: 2400,
+            image: 'https://www.detailxperts.net/wp-content/uploads/2018/03/Classic-cars.png',
+        },
+        {
+            id: '3',
+            name: 'W Shoes',
+            price: 1000,
+            image: 'https://cdn.shopify.com/s/files/1/1555/3859/products/bella-belle-shoes-embellished-ivory-crystal-wedding-heels-florence.jpg?v=1610575224',
+        },
+    ],
+};
+
+export default data;
